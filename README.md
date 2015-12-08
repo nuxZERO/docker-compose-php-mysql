f64cf0a4c919802393a2a94a4ae3637478a93234
Lattapon Yodsuwan <i@clozed2u.com>
2015-12-09 00:17:31 +0700

    Rewrite docker and docker-compose file, add sample php files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Hello
+###Hello
+From a docker side.
--- app/index.php
+++ app/index.php
@@ -1,1 +1,16 @@
-<?php phpinfo(); ?>
+<?php
+
+$user = 'docker';
+$pass = 'docker';
+$ip_address_of_docker_vm = '';
+
+try {
+  $dbh = new PDO("mysql:host=$ip_address_of_docker_vm;dbname=docker", $user, $pass);
+  print "Successfully connect to database<br>";
+  $dbh = null;
+} catch (PDOException $e) {
+  print "Error!: " . $e->getMessage() . "<br>";
+}
+
+print "<br>";
+print "Checkout <a href='info.php'>phpinfo()</a>";
